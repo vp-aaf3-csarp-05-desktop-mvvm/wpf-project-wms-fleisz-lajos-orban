49064558d7364a5467938d926abc2dc6aef80a39
Csav_R <116081238+CsavRR@users.noreply.github.com>
2026-02-03 10:40:07 +0100

    Feladat, az employee-nak legyen adószáma és legyen egy szám, a még nem kiadott szabadnapok száma, jelenítse meg őket!

--- WMSProjekt/EmployeeViewModel.cs
+++ WMSProjekt/EmployeeViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace WMSProjekt
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace WMSProjekt
 {
     public class EmployeeViewModel
     {
         public string Name { get; } = "Nagy Anna";
+        public string TaxID { get; } = "59712655-2-33";
+        public int DaysOff { get; } = 5;
     }
 }
