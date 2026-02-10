bb8cca0d87ea0aecbfb71fcecda111327ce6834c
Csav_R <116081238+CsavRR@users.noreply.github.com>
2026-02-10 10:24:54 +0100

    VIewModel három property-vel és ipari megjegyzésekkel

--- WMSProjekt/EmployeeViewModel.cs
+++ WMSProjekt/EmployeeViewModel.cs
@@ -2,10 +2,22 @@
 
 namespace WMSProjekt
 {
+    /// <summary>
+    /// Dolgozó adatok megjelenítéshez (szabadnapkezelés)
+    /// </summary>
     public class EmployeeViewModel
     {
+        /// <summary>
+        /// Dolgozó neve
+        /// </summary>
         public string Name { get; } = "Nagy Anna";
-        public string TaxID { get; } = "59712655-2-33";
-        public int DaysOff { get; } = 5;
+        /// <summary>
+        /// Adószáma
+        /// </summary>
+        public string TaxNumber { get; } = "59712655-2-33";
+        /// <summary>
+        /// Kivett szabadnapok száma
+        /// </summary>
+        public int DaysOff { get; } = 6;
     }
 }
